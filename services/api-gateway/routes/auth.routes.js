b7be2b2264c08a28b5e8c289db5ce289b301da51
BHUMISACHDEVA <bhumisachdeva2508004.com>
2026-04-01 17:12:37 +0530

    addingRouteofGoogleAuth

--- services/api-gateway/routes/auth.routes.js
+++ services/api-gateway/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, verifyEmail, login, requestPasswordReset, resetPassword, refreshToken } from "../controllers/auth.controller.js";
+import { signup, verifyEmail, login, requestPasswordReset, resetPassword, refreshToken, googleAuth } from "../controllers/auth.controller.js";
 import { authLimiter } from "../middlewares/ratelimiter.js";
 const router = express.Router();
 
@@ -9,4 +9,5 @@ router.post("/login",authLimiter,login);
 router.post("/password-reset-request",requestPasswordReset);
 router.post("/refresh",refreshToken);
 router.post("/password-reset-confirm",resetPassword);
+router.post("/google",googleAuth);
 export default router;
